63df9cd27e9c9cf51bc8a6a1ee113825a6fb35ee
Erdinc Tekay <erdinctekay@gmail.com>
2024-03-15 21:42:16 +0300

    added helmet, initialized cors config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(
+    helmet({
+      xPoweredBy: false
+    })
+  )
+  app.enableCors({
+    origin: [...process.env.ALLOWED_ORIGINS.split(' ')],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  })
   app.setGlobalPrefix('api/v1')
   await app.listen(process.env.PORT || 3000)
 }
